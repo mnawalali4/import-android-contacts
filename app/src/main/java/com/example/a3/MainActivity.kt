76bdcb1a7bb63751148631d004231f438312471d
mnawalali4 <mnawalali4@gmail.com>
2022-04-19 00:51:51 +0500

    day1 concluded

--- app/src/main/java/com/example/a3/MainActivity.kt
+++ app/src/main/java/com/example/a3/MainActivity.kt
@@ -2,16 +2,23 @@ package com.example.a3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<adaptar.ViewHolder>?=null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         layoutManager = LinearLayoutManager(this)
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
@@ -25,5 +32,11 @@ class MainActivity : AppCompatActivity() {
         }
         adapter = adaptar(mydatalist)
         recyclerview.adapter=adapter
+
+
     }
+
+
 }
+
+
