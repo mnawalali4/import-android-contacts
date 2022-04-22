c2a903a0a1199c541249da6f5a10e2e6f82277c0
mnawalali4 <mnawalali4@gmail.com>
2022-04-22 20:14:15 +0500

    before photo

--- app/src/main/java/com/example/a3/MainActivity.kt
+++ app/src/main/java/com/example/a3/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.a3
 
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -33,10 +41,44 @@ class MainActivity : AppCompatActivity() {
         adapter = adaptar(mydatalist)
         recyclerview.adapter=adapter
 
+        getcontacts()
+
 
     }
 
+    fun getcontacts()
+    {
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_CONTACTS),0);
+        }
+
+        var contentResolver: ContentResolver =getContentResolver()
+        var uri: Uri =ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+        var cursor: Cursor?=contentResolver.query(uri,null,null,null,null)
+
+        if (cursor != null) {
+            if(cursor.count >0)
+            {
+                Log.i("demo", "total contacts: " + Integer.toString(cursor.count))
+
+                while (cursor.moveToNext())
+                {
+                    var contactName: String =
+                        cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                    var contactNumber: String =
+                        cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    var address: String = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DATA))
+
+                    Log.i("demo", contactName+address)
+                }
+            }
+
+        }
+    }
 
 }
+
+
 
 
